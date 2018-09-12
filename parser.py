bb168be27418af8195099f409c865b52a54ade33
xnestea <juszczak.kajetan@gmail.com>
2018-09-12 07:15:20 +0200

    12.09

--- parser.py
+++ parser.py
@@ -1,15 +1,15 @@
+import pandas
+
 def parser1(infile):
     with open(infile, "r") as f:
-        c = 0
+        count = 0
         for line in f:
             line = line.split()
-            count = 0
-            for i in line:
-                print(count, i)
-                count += 1
-            c += 1
-            if c == 2:
+            print("fielname:{} drug:{} time:{} species:{} from {} {} dose:{}-{} {}".format(line[34],line[30],line[40],line[42],line[2],line[3],line[13],line[14],line[15]))
+            count += 1
+            if count == 20:
                 break
 
+    #file 34; drug 30; time 40; tissue - 42; species 2,3; dose 13,14,15
 if __name__ == "__main__":
     parser1("E-MTAB-799.sdrf.txt")
