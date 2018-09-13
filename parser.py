f04cf8fc9bfc75ed5d6e1e826576e6717d1b1d26
xNesTea <juszczak.kajetan@gmail.com>
2018-09-13 19:43:19 +0200

    13.09

--- parser.py
+++ parser.py
@@ -1,17 +1,12 @@
-import pandas
+import pandas as pd
 
-def parserhuman(infile):
-    with open(infile, "r") as f:
-        dictionary = {}
-        c = 0
-        for line in f:
-            count = 0
-            line = line.split()
-            ## add to pandas dataframe one collumn consisting of all info
-            c += 1
-            if c == 2:
-                break
-        
-    # hepatocytes human:: 2 - tissue 3,4 organism; 22 - dru; 26 - file, time - 32 , 33, dose - 11,12,13
+def parser(infile):
+    table = pd.read_csv(infile, sep="\t")
+    t = table.drop_duplicates(
+    subset=["Parameter Value[Compound]","Characteristics[CellType]", "Characteristics[Organism]"])
+    pivot = pd.pivot_table(t, 
+    index=["Parameter Value[Compound]"], aggfunc='count')
+    print(pivot)
+
 if __name__ == "__main__":
-    parser1("E-MTAB-798.sdrf.txt")
+    parser("metadata.txt")
